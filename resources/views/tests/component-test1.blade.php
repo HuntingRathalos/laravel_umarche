79b44ccd7cb7f3a7225efc0a4f6a214222d8673a
HuntingRathalos <huntingandrathalos@gmail.com>
2021-10-08 10:10:38 +0900

    変数

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -1,6 +1,6 @@
 <x-tests.app>
   <x-slot name="header">ヘッダー１</x-slot>
 
-<x-tests.card title="タイトル" content=”コンテント” />
+<x-tests.card title="タイトル" content=”コンテント” :message="$message" />
 コンポーネントテスト１
 </x-tests.app>
