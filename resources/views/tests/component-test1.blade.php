9c4e4648b53c8be0639807ac1c2c43fd69a50f1f
HuntingRathalos <huntingandrathalos@gmail.com>
2021-10-08 10:39:29 +0900

    属性バッグ

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -3,5 +3,6 @@
 
 <x-tests.card title="タイトル" content=”コンテント” :message="$message" />
 <x-tests.card title="タイトル2"  />
+<x-tests.card title="CSSを変更したい"  class="bg-red-300" />
 コンポーネントテスト１
 </x-tests.app>
