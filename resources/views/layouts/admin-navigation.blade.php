38331f51efd42a7c8a2d6e456f8b91a7485a9f37
HuntingRathalos <huntingandrathalos@gmail.com>
2021-10-09 17:41:04 +0900

    Carbon

--- resources/views/layouts/admin-navigation.blade.php
+++ resources/views/layouts/admin-navigation.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link :href="route('admin.dashboard')" :active="request()->routeIs('admin.dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('admin.owners.index')" :active="request()->routeIs('admin.owners.index')">
+                        オーナーいちらん
+                    </x-nav-link>
                 </div>
             </div>
 
