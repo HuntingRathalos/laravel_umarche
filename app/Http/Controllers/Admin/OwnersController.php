63a2cb1f0c7ef9993bdd9eaf1d8ab47d69707d70
HuntingRathalos <huntingandrathalos@gmail.com>
2021-10-11 13:48:09 +0900

    shopCreateDelete

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -9,7 +9,9 @@ use Illuminate\Support\Facades\DB; //クエリビルダ
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
-
+use Throwable;
+use Illuminate\Support\Facades\Log;
+use App\Models\Shop;
 
 
 class OwnersController extends Controller
@@ -61,11 +63,27 @@ class OwnersController extends Controller
             'password' => ['required', 'confirmed', Rules\Password::defaults()]
         ]);
 
-        Owner::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password)
-        ]);
+        try{
+            DB::transaction(function () use($request){
+                $owner = Owner::create([
+                    'name' => $request->name,
+                    'email' => $request->email,
+                    'password' => Hash::make($request->password)
+                ]);
+                Shop::create([
+                    'owner_id' => $owner->id,
+                    'name' => '店名を入力してください',
+                    'information' => '',
+                    'filename' => '',
+                    'is_selling' => true
+                ], 2);
+            });
+        }catch(Throwable $e){
+            Log::error();
+            throw $e;
+        }
+
+
 
         return redirect()
         ->route('admin.owners.index')
