1c288b43d51813ff90636ea6228cd8581981c12d
HuntingRathalos <huntingandrathalos@gmail.com>
2021-10-11 17:28:39 +0900

    intervention/image

--- config/app.php
+++ config/app.php
@@ -165,7 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        Intervention\Image\ImageServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -174,7 +174,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        
+
         App\Providers\SampleServiceProvider::class
 
     ],
@@ -230,6 +230,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'InterventionImage' => Intervention\Image\Facades\Image::class,
+
 
     ],
 
