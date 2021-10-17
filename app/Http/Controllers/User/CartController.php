4aa6d0034a952080bd82026aaed8f87120f403c2
HuntingRathalos <huntingandrathalos@gmail.com>
2021-10-18 01:09:36 +0900

    cartService作成

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -9,6 +9,7 @@ use App\Models\Cart;
 use App\Models\User;
 use App\Models\Stock;
 use Stripe;
+use App\Services\CartService;
 
 class CartController extends Controller
 {
@@ -50,6 +51,9 @@ class CartController extends Controller
 
     public function checkout()
     {
+        $items = Cart::where('user_id', Auth::id())->get();
+        $products = CartService::getItemsInCart($items);
+
         $user = User::findOrFail(Auth::id());
         $products = $user->products;
 
