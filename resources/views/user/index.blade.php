9e972fe0c98327720a0235dbc9e7eb173bd76a49
HuntingRathalos <huntingandrathalos@gmail.com>
2021-10-17 21:20:06 +0900

    検索フォーム　カテゴリー

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,11 +1,27 @@
 <x-app-layout>
   <x-slot name="header">
-    <div class="flex justify-between items-center">
       <h2 class="font-semibold text-xl text-gray-800 leading-tight">
           商品一覧
       </h2>
-      <div class="">
         <form method="get" action="{{ route('user.items.index')}}">
+          <div class="lg:flex lg:justify-around">
+            <div class="lg:flex items-center">
+              <select name="category" id="" class="mb-2 lg:mb-0 lg:mr-2">
+                <option value="0" @if (\Request::get('category') === '0') selected @endif>全て</option>
+                @foreach($categories as $category)
+                <optgroup label="{{ $category->name }}">
+                @foreach($category->secondary as $secondary)
+                  <option value="{{ $secondary->id}}" @if (\Request::get('category') == $secondary->id) selected @endif >
+                  {{ $secondary->name }}
+                  </option>
+                @endforeach
+                @endforeach
+              </select>
+              <div class="flex space-x-2 items-center">
+                <div class=""><input name="keyword" class="border border-gray-500 py-2" type="text" placeholder="キーワードを入力"></div>
+                <div class=""><button class="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">検索ボタン</button></div>
+              </div>
+            </div>
           <div class="flex items-center">
             <div class="">
               <span class="text-sm">表示順</span><br>
@@ -35,7 +51,6 @@
                 selected
                 @endif>古い順
                 </option>
-
                </select>
             </div>
             <div class="items-center">
@@ -60,9 +75,8 @@
               </select>
             </div>
           </div>
+        </div>
         </form>
-      </div>
-    </div>
   </x-slot>
 
   <div class="py-12">
